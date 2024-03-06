ca20e5cb7338bfb781e1c1214d3538e699870efd
Eugene <josumkin@yahoo.com>
2024-03-06 22:26:00 +0400

    build: deps updated

--- simple_task/src/app/app.component.ts
+++ simple_task/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-  title = 'simple_task';
+    title = "simple_task";
 }
